c497a30d891f0e572e61bfcafd742129ca87063d
mykola_seniuk <mseniuk@n-ix.com>
2018-09-13 17:07:09 +0300

    GENERAL: Added testcontainers dependency

--- src/integration-test/java/com/phone/PhoneControllerTest.java
+++ src/integration-test/java/com/phone/PhoneControllerTest.java
@@ -1,41 +1,30 @@
 package com.phone;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.util.Lists;
+import com.phone.catalog.Application;
+import com.phone.catalog.core.PhoneRepository;
+import com.phone.catalog.profile.Profiles;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
@@ -45,23 +34,15 @@ public class PhoneControllerTest {
 
     private static final int MYSQL_PORT = 3306;
 
-    private static final Long ORDER_ID_1 = 1L;
-    private static final String FIRST_NAME = "FirstName";
-    private static final String SECOND_NAME = "SecondName";
-    private static final String EMAIL = "test@gmail.email";
 
     @Autowired
     private WebApplicationContext context;
 
     @Autowired
-    private OrderRepository orderRepository;
+    private PhoneRepository orderRepository;
 
     @Mock
     private RestTemplate restTemplate;
-
-    @Autowired
-    @InjectMocks
-    private PhoneService phoneService;
 
     @ClassRule
     public static GenericContainer mysql = new GenericContainer("mysql:latest")
@@ -92,7 +73,8 @@ public class PhoneControllerTest {
 
     @Test
     public void shouldCreateOrder() throws Exception {
-
+        assertTrue(true
+        );
     }
 
 }
